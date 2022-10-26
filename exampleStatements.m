04ca88b0c3c587c85aaae8de968cc848c7ee7a37
Erick Pinzon <65203105+erickpinzon18@users.noreply.github.com>
2022-10-25 20:22:12 -0500

    Primera Parte
    Login Finalizado
    Registro Usuario finalizado

---
+++ exampleStatements.m
@@ -0,0 +1,25 @@
+clear;
+clc;
+
+% Create the connection with the ODBC connector previously created database in "databaseExplorer"
+conn = database('conn_scih', 'root', 'root');
+
+% Assign values ​​to a table and select all columns to use
+adminData = table("root","root123", "admin", "First Admin User", "admin@email.com", ...
+            'VariableNames', {'username', 'password', 'type', 'name', 'email'});
+basicData = table("basicUser","pass123", "basic", "First Basic User", "basic@email.com", ...
+            'VariableNames', {'username', 'password', 'type', 'name', 'email'});
+
+% Statement to execute a Sql Write to database in order: Connection, Table, Values
+%sqlwrite(conn, 'User', adminData);
+%sqlwrite(conn, 'User', basicData);
+%% Statements to get all columns of the table
+% Statement to execute a select on table User
+userValue = sqlread(conn, 'User');
+%% Statements to get specific columns and add WHERE to Query
+% Prepare the query sentence 
+query = strcat("SELECT password, type FROM User ", ...
+    "WHERE username = 'root'");
+rslt = fetch(conn, query);
+%% Close connection
+close(conn);
